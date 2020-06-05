339938560d251de86b5d3a3b0edb834f956e56ed
Marie Morin <mariemorin19@gmail.com>
2020-06-05 11:04:40 +0200

    Creation de la structure du système de tabs de la section seasons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log("hello");
+const tabs = document.querySelectorAll(".tabs a");
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", function (e) {
+        const li = this.parentNode;
+        const div = this.parentNode.parentNode.parentNode;
+        const actif = div.querySelector(".tabs .active");
+        // On sort de la fonction si l'onglet cliqué est déjà l'onglet actif
+        if (li.classList.contains("active")) {
+            return false;
+        }
+        // On retire la class active sur l'onglet qui l'avait
+        actif.classList.remove("active");
+        // On ajoute à la class active sur l'onglet cliqué
+        tab.classList.add("active");
+        // On retire la class active sur le contenu qui l'avait
+        div.querySelector(".tab__content.active").classList.remove("active");
+        // On ajoute la class active sur le contenu qui correspond à l'onglet cliqué
+        div.querySelector(this.getAttribute("href")).classList.add("active");
+    })
+})
